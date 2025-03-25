db9e63a387c83a9311530366e4527b971de10f51
cafeyen21 <cafeyen21@gmail.com>
2025-03-24 21:34:06 -0500

    fixed some clippy warnings in tests

--- src/applier/matcher/len_tests.rs
+++ src/applier/matcher/len_tests.rs
@@ -4,19 +4,19 @@ use super::*;
 
 #[test]
 fn empty() {
-    assert_eq!(Ok(0), match_len(&vec![], &Choices::default()));
+    assert_eq!(Ok(0), match_len(&[], &Choices::default()));
 }
 
 #[test]
 fn phone() {
-    assert_eq!(Ok(1), match_len(&vec![
+    assert_eq!(Ok(1), match_len(&[
         RuleToken::Phone(Phone::new("a")),
     ], &Choices::default()));
 }
 
 #[test]
 fn three_phoens() {
-    assert_eq!(Ok(3), match_len(&vec![
+    assert_eq!(Ok(3), match_len(&[
         RuleToken::Phone(Phone::new("a")),
         RuleToken::Phone(Phone::new("b")),
         RuleToken::Phone(Phone::new("c")),
@@ -25,7 +25,7 @@ fn three_phoens() {
 
 #[test]
 fn gap() {
-    assert_eq!(Err(MatchError::CannotCheckLenOfGap), match_len(&vec![
+    assert_eq!(Err(MatchError::CannotCheckLenOfGap), match_len(&[
         RuleToken::Gap { id: None },
     ], &Choices::default()));
 }
@@ -37,7 +37,7 @@ fn unlabeled_optional() {
         RuleToken::Phone(Phone::new("b")),
     ] };
 
-    assert_eq!(Err(MatchError::UnlabeledScope(&scope.clone())), match_len(&vec![
+    assert_eq!(Err(MatchError::UnlabeledScope(&scope.clone())), match_len(&[
         scope,
     ], &Choices::default()));
 }
@@ -45,27 +45,27 @@ fn unlabeled_optional() {
 #[test]
 fn inserted_optional() {
     let mut choices = Choices::default();
-    choices.optional_choices = HashMap::from_iter([(&ScopeId::Name("label"), true)].into_iter());
+    choices.optional_choices = HashMap::from_iter([(&ScopeId::Name("label"), true)]);
 
     let scope = RuleToken::OptionalScope { id: Some(ScopeId::Name("label")), content: vec![
         RuleToken::Phone(Phone::new("a")),
         RuleToken::Phone(Phone::new("b")),
     ] };
 
-    assert_eq!(Ok(2), match_len(&vec![scope], &choices));
+    assert_eq!(Ok(2), match_len(&[scope], &choices));
 }
 
 #[test]
 fn non_inserted_optional() {
     let mut choices = Choices::default();
-    choices.optional_choices = HashMap::from_iter([(&ScopeId::Name("label"), false)].into_iter());
+    choices.optional_choices = HashMap::from_iter([(&ScopeId::Name("label"), false)]);
 
     let scope = RuleToken::OptionalScope { id: Some(ScopeId::Name("label")), content: vec![
         RuleToken::Phone(Phone::new("a")),
         RuleToken::Phone(Phone::new("b")),
     ] };
 
-    assert_eq!(Ok(0), match_len(&vec![scope], &choices));
+    assert_eq!(Ok(0), match_len(&[scope], &choices));
 }
 
 #[test]
@@ -75,7 +75,7 @@ fn unlabeled_selection() {
         vec![RuleToken::Phone(Phone::new("b")), RuleToken::Phone(Phone::new("c"))],
     ] };
 
-    assert_eq!(Err(MatchError::UnlabeledScope(&scope.clone())), match_len(&vec![
+    assert_eq!(Err(MatchError::UnlabeledScope(&scope.clone())), match_len(&[
         scope,
     ], &Choices::default()));
 }
@@ -83,36 +83,36 @@ fn unlabeled_selection() {
 #[test]
 fn first_in_selection() {
     let mut choices = Choices::default();
-    choices.selection_choices = HashMap::from_iter([(&ScopeId::Name("label"), 0)].into_iter());
+    choices.selection_choices = HashMap::from_iter([(&ScopeId::Name("label"), 0)]);
 
     let scope = RuleToken::SelectionScope { id: Some(ScopeId::Name("label")), options: vec![
         vec![RuleToken::Phone(Phone::new("a"))],
         vec![RuleToken::Phone(Phone::new("b")), RuleToken::Phone(Phone::new("c"))],
     ] };
 
-    assert_eq!(Ok(1), match_len(&vec![scope], &choices));
+    assert_eq!(Ok(1), match_len(&[scope], &choices));
 }
 
 #[test]
 fn second_in_selection() {
     let mut choices = Choices::default();
-    choices.selection_choices = HashMap::from_iter([(&ScopeId::Name("label"), 1)].into_iter());
+    choices.selection_choices = HashMap::from_iter([(&ScopeId::Name("label"), 1)]);
 
     let scope = RuleToken::SelectionScope { id: Some(ScopeId::Name("label")), options: vec![
         vec![RuleToken::Phone(Phone::new("a"))],
         vec![RuleToken::Phone(Phone::new("b")), RuleToken::Phone(Phone::new("c"))],
     ] };
 
-    assert_eq!(Ok(2), match_len(&vec![scope], &choices));
+    assert_eq!(Ok(2), match_len(&[scope], &choices));
 }
 
 #[test]
 fn any_equal_phone() {
 
 
-    assert_eq!(match_len(&vec![
+    assert_eq!(match_len(&[
         RuleToken::Any { id: Some(ScopeId::IOUnlabeled { id_num: 0, label_type: LabelType::Any, parent: None }) },
-    ], &Choices::default()), match_len(&vec![
+    ], &Choices::default()), match_len(&[
         RuleToken::Phone(Phone::new("a")),
     ], &Choices::default()));
 }
