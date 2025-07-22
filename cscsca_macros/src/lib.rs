b9164253cf268916f7e45ee9b017fb88dc41c5c1
cafeyen21 <cafeyen21@gmail.com>
2025-07-22 17:07:07 -0500

    cscsca_macros now has no dependencies when the async_io flag is inactive to optimize compile times

--- cscsca_macros/src/lib.rs
+++ cscsca_macros/src/lib.rs
@@ -1,20 +1,35 @@
 use proc_macro::TokenStream;
+#[cfg(feature = "async_io")]
 use proc_macro2::TokenStream as TokenStream2;
+#[cfg(feature = "async_io")]
 use quote::{quote, ToTokens};
+#[cfg(feature = "async_io")]
 use syn::parse::{Parse, ParseStream};
 
+
+#[cfg(not(feature = "async_io"))]
+/// Makes a function asyncronous if the `async_io` feature flag is active
+#[proc_macro_attribute]
+pub fn io_fn(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    item
+}
+
+#[cfg(feature = "async_io")]
 /// Makes a function asyncronous if the `async_io` feature flag is active
 #[proc_macro_attribute]
 pub fn io_fn(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let io_fn = syn::parse_macro_input!(item as IoFn);
     quote! { #io_fn }.into()
 }
+
+#[cfg(feature = "async_io")]
 struct IoFn {
     attrs: Vec<syn::Attribute>,
     public: bool,
     fn_rest: TokenStream2,
 }
 
+#[cfg(feature = "async_io")]
 impl Parse for IoFn {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         let attrs = input.call(syn::Attribute::parse_outer)?;
@@ -25,6 +40,7 @@ impl Parse for IoFn {
     }
 }
 
+#[cfg(feature = "async_io")]
 impl ToTokens for IoFn {
     fn to_tokens(&self, tokens: &mut TokenStream2) {
         let Self {attrs,  public, fn_rest } = self;
@@ -37,14 +53,9 @@ impl ToTokens for IoFn {
             quote! {}
         };
 
-        #[cfg(feature = "async_io")]
-        let iosync = quote! { async };
-        #[cfg(not(feature = "async_io"))]
-        let iosync = quote! {};
-
         tokens.extend(quote! {
             #(#attrs)*
-            #public #iosync fn #fn_rest
+            #public async fn #fn_rest
         });
     }
 }
